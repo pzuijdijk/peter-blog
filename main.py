9d213a35e66fbae5c2a929c3333117c3cc4e9402
Peter Zuijdijk <pzuijdijk@gmail.com>
2021-04-24 19:33:07 +0200

    bug in edit_post

--- main.py
+++ main.py
@@ -240,7 +240,7 @@ def edit_post(post_id):
         post.title = edit_form.title.data
         post.subtitle = edit_form.subtitle.data
         post.img_url = edit_form.img_url.data
-        post.author = edit_form.author.data
+        # post.author = current_user
         post.body = edit_form.body.data
         db.session.commit()
         return redirect(url_for("show_post", post_id=post.id, current_user=current_user))
